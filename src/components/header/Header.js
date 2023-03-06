fc5c044b04cfa3e751b9923804213303b1a9851f
Mitar1605 <120776088+Mitar1605@users.noreply.github.com>
2023-03-06 19:12:12 +0400

    modified

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import {FcLike} from 'react-icons/fc'
+import {AiOutlineShoppingCart} from 'react-icons/ai'
+import {BiUserCircle} from 'react-icons/bi'
 import './Header.css'
 
 export default function Header() {
   return (
     <header>
-        <div className="header-container">
+        <div className="header_container">
             <div className="logo_div">
-                <h4>BeOnline</h4>
+                <NavLink to='/'>BeOnline</NavLink>
             </div>
-            
+            <div className="haeder_content">
+              <nav>
+                <ul>
+                  <li><NavLink to='/'><FcLike /></NavLink></li>
+                  <li><NavLink to='/'><AiOutlineShoppingCart /></NavLink></li>
+                  <li><NavLink to='/'><BiUserCircle /> <span>Մուտք</span></NavLink></li>
+                </ul>
+              </nav>
+            </div>
         </div>
     </header>
   )
