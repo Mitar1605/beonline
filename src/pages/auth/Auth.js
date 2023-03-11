5fbddbbdf668961c8c58922f82b9902f11438b8b
Mitar1605 <120776088+Mitar1605@users.noreply.github.com>
2023-03-11 12:52:49 +0400

    modified

--- src/pages/auth/Auth.js
+++ src/pages/auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { memo, useState, useContext } from 'react'
+import React, { memo, useState, useContext, useEffect } from 'react'
 import {Link} from 'react-router-dom'
 import useFetch from '../../hooks/useFetch'
 import { isAuthContext } from '../../App'
@@ -11,55 +11,60 @@ export default memo(function Auth() {
 
   const [isRegister, setIsRegister] = useState(!isAuth)
 
-  const [emailText, setEmailText] = useState('')
-  const [pswrdText, setPswrdText] = useState('')
   const [remeberMe, setRememberMe] = useState(false);
-
-  const [pswrdLength, setPswrdLength] = useState(0)
- 
-  const changeText = (e, state) => {
-    state(e.target.value)
-    state === setPswrdText && setPswrdLength(e.target.value.length)
-  }
 
   const {data} = useFetch('http://localhost:3500/users')
 
   const users = [].concat(data)
 
+  const [post, setPost] = useState(
+    {
+      id: users.length,
+      email: '',
+      password: '',
+      remeber: remeberMe
+    }
+  )
+
+  const newPut = {
+    id: 'aaaa',
+    email: 'uuuu',
+    password: 'asasdsa',
+    remeberMe: 'asdsadsa'
+  }
+
+  const handle = (event) => {
+    const newData = {...post}
+    newData[event.target.name] = event.target.value
+    setPost(newData)
+  }
+
+  useEffect(() => {
+    const newData = {...post}
+    newData.remeber = remeberMe
+    setPost(newData)
+  }, [remeberMe])
+
   return (
     <div className='auth_main'>
       <form onSubmit={(e) => {
         e.preventDefault()
-        if (isRegister && !users.some(user => user.email === emailText) && pswrdLength >= 8) {
-          Axios.post('http://localhost:3500/users', [
-            ...users,
-            {
-              id: users.length,
-              email: emailText,
-              password: pswrdText,
-              remeber: remeberMe
-            }
-          ])
-
+        if (isRegister && !users.some(user => user.email === post.email) && post.password.length >= 8) {
+          Axios.post('http://localhost:3500/users', post)
           window.location = 'http://localhost:3000/'
 
           remeberMe && localStorage.setItem('isAuth', JSON.stringify({val: true}))
 
-          remeberMe && localStorage.setItem('rememberUser', JSON.stringify({
-            id: users.length,
-            email: emailText,
-            password: pswrdText,
-            remeber: remeberMe
-          }))
+          remeberMe && localStorage.setItem('rememberUser', JSON.stringify(post))
           
-        }else if (isRegister && users.some(user => user.email === emailText)) {
+        }else if (isRegister && users.some(user => user.email === post.email)) {
           alert('Այս էլ․ հասցեն արդեն գրանցված է')
-        }else if (pswrdLength <= 8) {
+        }else if (post.password.length <= 8) {
           alert('Գաղտնաբառը պետք է պարունակի ամենաքիչը 8 նիշ')
         }else if (!isRegister) {
-          if (users.some(user => user.email === emailText)) {
-            const user = users.find(user => user.email === emailText)
-            if (user.password === pswrdText) {
+          if (users.some(user => user.email === post.email)) {
+            const user = users.find(user => user.email === post.email)
+            if (user.password === post.password) {
               localStorage.setItem('isAuth', JSON.stringify({val: true}))
               localStorage.setItem('rememberUser', JSON.stringify(user))
 
@@ -79,16 +84,16 @@ export default memo(function Auth() {
         </div>
         <label>
             Էլ․ Հասցե
-            <input type="email" placeholder='Մուտքագրեք ձեր էլ․ հասցեն' required title='Էլ․ Հասցե' onChange={(e) => changeText(e, setEmailText)} />
+            <input type="email" name='email' placeholder='Մուտքագրեք ձեր էլ․ հասցեն' required title='Էլ․ Հասցե' onChange={(e) => handle(e)} />
         </label>
         <label>
             Գաղտանաբառ
-            <input type="password" placeholder='********' required title='Գաղտանաբառ' onChange={(e) => changeText(e, setPswrdText)} />
+            <input type="password" name='password' placeholder='********' required title='Գաղտանաբառ' onChange={(e) => handle(e)} />
         </label>
 
         <div className="form_password_remember">
             <label className='form_password_remember_label' >
-                <input type="checkbox" disabled onChange={() => setRememberMe(remeberMe ? false: true)} /> Հիշել տվյալներս
+                <input type="checkbox" onChange={() => setRememberMe(remeberMe ? false: true)} /> Հիշել տվյալներս
             </label>
             <Link to='/'>Մոռացել եմ գաղտնաբառս</Link>
         </div>
